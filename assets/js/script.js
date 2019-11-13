44dcd6a052461b7d1d5f98a4a2f65c870b74c2a6
chrisward90 <chrsward90@gmail.com>
2019-11-12 18:08:59 -0800

    Added flip card functionality to two cards and added lfz-background to body

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
+$(document).ready(initializeApp);
 
+{/* <div class="card">
+  <div class="front lfz-card"></div>
+  <div class="back js-logo"></div>
+</div> */}
+
+function initializeApp() {
+  $(".card").on("click", handleCardClick);
+}
+
+function handleCardClick(event) {
+  var currentTarget = $(event.currentTarget);
+  currentTarget.find(".front").addClass("hidden");
+  currentTarget.find(".back").removeClass("hidden");
+}
