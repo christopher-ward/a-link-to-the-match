879bb3ee749a113f4677741c6ffb9dfa056bf473
chrisward90 <chrsward90@gmail.com>
2019-11-14 10:38:31 -0800

    Win condition achieved and fixed bug that had double click on same card count as match.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,19 @@ $(document).ready(initializeApp);
 var firstCardClicked = null;
 var secondCardClicked = null;
 var matches = null;
-
-
+var maxMatches = 9;
 
 function initializeApp() {
   $(".card").on("click", handleCardClick);
+
 }
 
 function handleCardClick(event) {
   var currentCard = $(event.currentTarget);
 
+  if (currentCard.find(".back").hasClass("hidden")) {
+    return;
+  }
   currentCard.find(".back").addClass("hidden");
 
   if (firstCardClicked === null) {
@@ -25,14 +28,36 @@ function handleCardClick(event) {
     var firstClickImage = firstCardClicked.find(".front").css("background-image");
     var secondClickImage = secondCardClicked.find(".front").css("background-image");
 
-    if (firstClickImage === secondClickImage) {
-      console.log("Cards match!");
-      matches++;
-    } else {
-      setTimeout(function(){ //first arg in setTimeout is anon function with the elements to be hidden
+    if (firstClickImage != secondClickImage) {
+      $(".card").off("click", handleCardClick);
+      setTimeout(function () { //first arg in setTimeout is anon function with the elements to be hidden
         firstCardClicked.find(".back").removeClass("hidden");
         secondCardClicked.find(".back").removeClass("hidden");
+        firstCardClicked = null;
+        secondCardClicked = null;
+        $(".card").on("click", handleCardClick);
       }, 1500);
+    } else {
+      console.log("Cards match!");
+      matches++;
+      if (matches === maxMatches) {
+        winConditionModal();
+      }
+      firstCardClicked = null;
+      secondCardClicked = null;
     }
   }
+  $("#winDiv").on("click", function () {
+    hideModal();
+  });
+
+}
+
+function winConditionModal() {
+  $("#tryAgainDiv").show();
+  $("#winDiv").show();
+}
+function hideModal() {
+  $("#winDiv").hide();
+  $("#tryAgainDiv").hide();
 }
