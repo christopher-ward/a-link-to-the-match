9c10848b2bdb5a27694b1db1618162e25d0472b7
chrisward90 <chrsward90@gmail.com>
2019-11-13 17:25:11 -0800

    Fixed html and css for proper effect to occur and created matching and mismatching pairs features

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,38 @@
 $(document).ready(initializeApp);
 
-{/* <div class="card">
-  <div class="front lfz-card"></div>
-  <div class="back js-logo"></div>
-</div> */}
+var firstCardClicked = null;
+var secondCardClicked = null;
+var matches = null;
+
+
 
 function initializeApp() {
   $(".card").on("click", handleCardClick);
 }
 
 function handleCardClick(event) {
-  var currentTarget = $(event.currentTarget);
-  currentTarget.find(".front").addClass("hidden");
-  currentTarget.find(".back").removeClass("hidden");
+  var currentCard = $(event.currentTarget);
+
+  currentCard.find(".back").addClass("hidden");
+
+  if (firstCardClicked === null) {
+    firstCardClicked = currentCard;
+  } else {
+    secondCardClicked = currentCard;
+  }
+
+  if (secondCardClicked) {
+    var firstClickImage = firstCardClicked.find(".front").css("background-image");
+    var secondClickImage = secondCardClicked.find(".front").css("background-image");
+
+    if (firstClickImage === secondClickImage) {
+      console.log("Cards match!");
+      matches++;
+    } else {
+      setTimeout(function(){ //first arg in setTimeout is anon function with the elements to be hidden
+        firstCardClicked.find(".back").removeClass("hidden");
+        secondCardClicked.find(".back").removeClass("hidden");
+      }, 1500);
+    }
+  }
 }
